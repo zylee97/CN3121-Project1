29c0298847be5946e3904c894afea590e5d7a70b
zylee97 <zylee97@gmail.com>
2020-10-03 16:06:50 +0800

    Update CN3121Project1.m

--- CN3121Project1.m
+++ CN3121Project1.m
@@ -17,6 +17,8 @@ z = sol.z
 timeperiod = 0:0.1:200; y0 = [3.48603 0.716788 5.08040];
 
 %%+30%, -30% step change for Cgin for Cg
+
+[t, y1] = ode45(@(t,y) ConcODE1(t,y), timeperiod, y0);
 [t, y13] = ode45(@(t,y) ConcODE13(t,y), timeperiod, y0);
 [t, y07] = ode45(@(t,y) ConcODE07(t,y), timeperiod, y0);
 
@@ -25,26 +27,41 @@ hold off
 plot(t,y13(:,1)), title('Step change in Cm+30% step change in blue, -30% step change in red')
 hold on
 plot(t,y07(:,1))
+plot(t,y1(:,1))
 
-figure(2)
-hold off
-plot(t,y13(:,2)), title('Step change in Ce+30% step change in blue, -30% step change in red')
-hold on
-plot(t,y07(:,2))
+% figure(2)
+% hold off
+% plot(t,y13(:,2)), title('Step change in Ce+30% step change in blue, -30% step change in red')
+% hold on
+% plot(t,y07(:,2))
+% 
+% figure(3)
+% hold off
+% plot(t,y13(:,3)), title('Step change in Cg+30% step change in blue, -30% step change in red')
+% hold on
+% plot(t,y07(:,3))
+% 
+% [t, y131] = ode45(@(t,y) conclinearODE13(t,y), timeperiod, y0);
+% [t, y071] = ode45(@(t,y) conclinearODE07(t,y), timeperiod, y0);
+% figure(4)
+% hold off
+% plot(t,y131(:,1)), title('step change linear, Cm')
+% hold on
+% plot(t,y071(:,1))
+end
 
-figure(3)
-hold off
-plot(t,y13(:,3)), title('Step change in Cg+30% step change in blue, -30% step change in red')
-hold on
-plot(t,y07(:,3))
+function dy = ConcODE1(t,y)
+Ke=0.5; Ke1=2; Kg=1.03; Kg1=1.68; V=1000; F=50; Cgin=10; n1=1; n2=0.5; um=0.3; ue=0.2;
 
-[t, y131] = ode45(@(t,y) conclinearODE13(t,y), timeperiod, y0);
-[t, y071] = ode45(@(t,y) conclinearODE07(t,y), timeperiod, y0);
-figure(4)
-hold off
-plot(t,y131(:,1)), title('step change linear, Cm')
-hold on
-plot(t,y071(:,1))
+Cm = y(1);
+Ce = y(2); 
+Cg = y(3);
+dCm = um*(Cg/(Kg+Cg))*exp(-Ke*Ce) - (F/V)*Cm;
+dCe = ue*(Cg/(Kg1+Cg))*exp(-Ke1*Ce) - (F/V)*Ce;
+dCg = -um*(Cg/(Kg+Cg))*exp(-Ke*Ce) - 2*ue*(Cg/(Kg1+Cg))*exp(-Ke*Ce) + (F/V)*1*Cgin - (F/V)*Cg;
+
+dy = [dCm dCe dCg]';
+
 end
 
 function dy = ConcODE13(t,y)
