e708e52c1ce07031585236e580308583b3d1797e
faizudeen1997 <faizudeen1997@gmail.com>
2020-10-11 16:09:37 +0800

    Update CN3121Project1.m

--- CN3121Project1.m
+++ CN3121Project1.m
@@ -7,62 +7,66 @@ syms Cm Ce Cg
 eqns = [um*(Cg/(Kg+Cg))*exp(-Ke*Ce) - (F/V)*Cm == 0; ue*(Cg/(Kg1+Cg))*exp(-Ke1*Ce) - (F/V)*Ce == 0 ; -um*(Cg/(Kg+Cg))*exp(-Ke*Ce) - 2*ue*(Cg/(Kg1+Cg))*exp(-Ke1*Ce) + (F/V)*Cgin - (F/V)*Cg == 0];
 
 soln = solve(eqns, [Cm Ce Cg]);
-SCm = soln.Cm
-SCe = soln.Ce
-SCg = soln.Cg
+SCm = soln.Cm;
+SCe = soln.Ce;
+SCg = soln.Cg;
 
 %% (b)
 
 timeperiod = 0:0.1:200; y0 = [3.48603 0.716788 5.08040];
+
+Ke=0.5; Ke1=2; Kg=1.03; Kg1=1.68; V=1000; F=50; n1=1; n2=0.5; um=0.3; ue=0.2;
 
 %+30%, -30% step change for Cgin for Cg
 [t, y13] = ode45(@(t,y) ConcODE13(t,y), timeperiod, y0);
 [t, y07] = ode45(@(t,y) ConcODE07(t,y), timeperiod, y0);
 
 figure (1) %help
-
 hold off
-plot(t,y13(:,1)), title('Step change in Cm+30% step change in blue, -30% step change in red')
+plot(t,y13(:,1)), title('Change in Cm: +30% step in blue, -30% step in red')
 hold on
 plot(t,y07(:,1))
 
 figure(2)
 hold off
-plot(t,y13(:,2)), title('Step change in Ce+30% step change in blue, -30% step change in red')
+plot(t,y13(:,2)), title('Change in Ce: +30% step in blue, -30% step in red')
 hold on
 plot(t,y07(:,2))
 
 figure(3)
 hold off
-plot(t,y13(:,3)), title('Step change in Cg+30% step change in blue, -30% step change in red')
+plot(t,y13(:,3)), title('Change in Cg: +30% step in blue, -30% step in red')
 hold on
 plot(t,y07(:,3))
 
-Ke=0.5; Ke1=2; Kg=1.03; Kg1=1.68; V=1000; F=50; Cgin=10; n1=1; n2=0.5; um=0.3; ue=0.2;
+timeperiod = 0:0.1:200; y0 = [3.48603 0.716788 5.08040 10];
 
-dcmdt = @(Cm, Ce, Cg) (um*(Cg/(Kg+Cg))*exp(-Ke*Ce) - (F/V)*Cm);
-dcedt = @(Cm, Ce, Cg) (ue*(Cg/(Kg1+Cg))*exp(-Ke1*Ce) - (F/V)*Ce);
-dcgdt = @(Cm, Ce, Cg) (-um*(Cg/(Kg+Cg))*exp(-Ke*Ce) - 2*ue*(Cg/(Kg1+Cg))*exp(-Ke1*Ce) + (F/V)*Cgin - (F/V)*Cg);
-
-syms Cm Ce Cg Cgin c1 c2 c3 c4
-
-dcmprimedt = diff(dcmdt, Cm)*c1 + diff(dcmdt, Ce)*c2 + diff(dcmdt, Cg)*c3 + diff(dcmdt, Cgin)*c4;
-dceprimedt = diff(dcedt, Cm)*c1 + diff(dcedt, Ce)*c2 + diff(dcedt, Cg)*c3 + diff(dcedt, Cgin)*c4;
-dcgprimedt = diff(dcgdt, Cm)*c1 + diff(dcgdt, Ce)*c2 + diff(dcgdt, Cg)*c3 + diff(dcgdt, Cgin)*c4;
-
-dcmprimedt = subs(dcmprimedt,{Cm Ce Cg Cgin},{SCm SCe SCg 13});
-dceprimedt = subs(dceprimedt,{Cm Ce Cg Cgin},{SCm SCe SCg 13});
-dcgprimedt = subs(dcgprimedt,{Cm Ce Cg Cgin},{SCm SCe SCg 13});
-
-timeperiod = 0:0.1:200; y0 = [3.48603 0.716788 5.08040];
-
-[t,c] = ode45(@(t,c) ConcODElin13(t,c), timeperiod, y0);
+[t,y13lin] = ode45(@(t,y) ConcODElin(t,y,13), timeperiod, y0);
+[t,y07lin] = ode45(@(t,y) ConcODElin(t,y,7), timeperiod, y0);
 
 figure (4) %help
-
 hold off
-plot(t, c(:,1)), title('lin')
+plot(t, y13lin(:,1)), title('Linearized change in Cm: +30% blue, -30% red')
 hold on
+plot(t, y07lin(:,1))
+
+figure (5) %help
+hold off
+plot(t, y13lin(:,2)), title('Linearized change in Ce: +30% blue, -30% red')
+hold on
+plot(t, y07lin(:,2))
+
+figure (6) %help
+hold off
+plot(t, y13lin(:,3)), title('Linearized change in Cg: +30% blue, -30% red')
+hold on
+plot(t, y07lin(:,3))
+
+figure(7)   % Test graph to see if y(4) = Cgin is at the correct value
+hold off
+plot(t, y13lin(:,4)), title('Cgin')
+hold on
+plot(t, y07lin(:,4))
 
 end
 
@@ -94,9 +98,20 @@ dy = [dCm dCe dCg]';
 
 end
 
-function dcdt = ConcODElin13(t,c)
-dcdt(1) =0.0057832399879726810623816239105383*c(3) - 0.087150693089666726210514657375972*c(2) - c(1)/20;
-dcdt(2) =0.0017530754178433184473879919756295*c(3) - 0.12167883653828177255771137200661*c(2);
-dcdt(3) =0.23050836616623027132593740138919*c(2) - 0.059289390823659317957157607861797*c(3);
-dcdt = [dcdt(1);dcdt(2);dcdt(3)];
+function dydt = ConcODElin(t,y,Cgin)
+dydt = [0; 0; 0; 0];
+Ke=0.5; Ke1=2; Kg=1.03; Kg1=1.68; V=1000; F=50; n1=1; n2=0.5; um=0.3; ue=0.2;
+y0 = [3.48603 0.716788 5.08040 10];
+y(4) = Cgin;
+
+dydt(1) = -(F/V)*(y(1)-y0(1))...
+        - Ke*um*y0(3)/(Kg+y0(3))*exp(-Ke*y0(2))*(y(2)-y0(2))...
+        + um*exp(-Ke*y0(2))*Kg/((y0(3)+Kg)^2)*(y(3)-y0(3));
+dydt(2) = (-Ke1*ue*y0(3)/(Kg1+y0(3))*exp(-Ke1*y0(2))-(F/V))*(y(2)-y0(2))...
+        + ue*exp(-Ke1*y0(2))*Kg1/((y0(3)+Kg1)^2)*(y(3)-y0(3));
+dydt(3) = (Ke/n1*um*y0(3)/(Kg+y0(3))*exp(-Ke*y0(2))+Ke1/n2*ue*y0(3)/(Kg1+y0(3))*exp(-Ke1*y0(2)))*(y(2)-y0(2))...
+        + (-um/n1*exp(-Ke*y0(2))*Kg/((Kg+y0(3))^2)-ue/n2*exp(-Ke1*y0(2))*Kg1/((Kg1+y0(3))^2)-F/V)*(y(3)-y0(3))...
+        + (F/V)*(y(4)-y0(4));
+dydt(4) = 0;
+
 end
