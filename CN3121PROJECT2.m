6993571fb4969df4d293ca15a1ee345f794de3dc
zylee97 <zylee97@gmail.com>
2020-11-18 19:12:03 +0800

    Update CN3121PROJECT2.m

--- CN3121PROJECT2.m
+++ CN3121PROJECT2.m
@@ -7,41 +7,74 @@ kCitae = (0.586/K)*(theta/2.5)^(-0.916); tauIitae = 2.5/(1.03-0.165*theta/tau);
 
 s=tf('s');
 
-% gOLds = kCds*(1+(1/tauIds/s))*(2*exp(-theta*s)/(2.5*s+1));
-% gOLitae = kCitae*(1+(1/tauIitae/s))*(2*exp(-theta*s)/(2.5*s+1));
+% gOLds = kCds*(1+(1/tauIds/s))*(K*exp(-theta*s)/(2.5*s+1));
+% gOLitae = kCitae*(1+(1/tauIitae/s))*(K*exp(-theta*s)/(2.5*s+1));
 % %bode(gOLds)
 % 
 % [gmds,pmds,wcds,wgds] = margin(gOLds)
 % [gmitae,pmitae,wcitae,wgitae] = margin(gOLitae)
 
 %% d)
+% flag = false;
+% addK = 0.005; %for accuracy up to 2dp
+% Kold = K;
+% 
+% while flag==false
+%     K=K+addK;
+%     gOLds = kCds*(1+(1/tauIds/s))*(K*exp(-theta*s)/(2.5*s+1));
+%     
+%     [gmds,pmds,wcds,wgds] = margin(gOLds);
+%     
+%     if 1/gmds > 1 break
+%     end
+% end
+% kMaxDS = K;
+% 
+% K=Kold;
+% flag == false;
+% addK=0.005;
+% while flag==false
+%     K=K+addK;
+%     gOLitae = kCitae*(1+(1/tauIitae/s))*((K*exp(-theta*s))/(2.5*s+1));
+%     [gmitae,pmitae,wcitae,wgitae] = margin(gOLitae);
+%  
+%     if 1/gmitae > 1 break
+%     end
+% end
+% kMaxITAE = K;   
+% 
+% [kMaxDS-0.005 kMaxITAE-0.005]
+
+%% e
 flag = false;
-addK = 0.005; %for accuracy up to 2dp
+addTheta = 0.005; %for accuracy up to 2dp
+thetaOld = theta;
 
 while flag==false
-    kCds=kCds+addK;
-    gOLds = kCds*(1+(1/tauIds/s))*(2*exp(-theta*s)/(2.5*s+1));
+    theta=theta+addTheta;
+    gOLds = kCds*(1+(1/tauIds/s))*(K*exp(-theta*s)/(2.5*s+1));
     
     [gmds,pmds,wcds,wgds] = margin(gOLds);
     
     if 1/gmds > 1 break
     end
 end
+thetaMaxDS = theta;
 
-A=1;
-
+theta = thetaOld;
 flag == false;
 addK=0.005;
 while flag==false
-    kCitae=kCitae+addK;
-    gOLitae = kCitae*(1+(1/tauIitae/s))*(2*exp(-theta*s)/(2.5*s+1));
+    theta=theta+addTheta;
+    gOLitae = kCitae*(1+(1/tauIitae/s))*((K*exp(-theta*s))/(2.5*s+1));
     [gmitae,pmitae,wcitae,wgitae] = margin(gOLitae);
  
     if 1/gmitae > 1 break
     end
 end
-    
-[kCds-0.005 kCitae-0.005]
+thetaMaxITAE = theta;   
+
+[thetaMaxDS-0.005 thetaMaxITAE-0.005]
 
 end
 
